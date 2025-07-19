a59df92ed0673eff966e1ae296f84cb04f0adf8a
gitjainnew <gitjain09@gmail.com>
2025-07-19 22:31:50 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
-html_css_files = ['custom.css']
+html_css_files = ['style.css']
 html_sidebars = {'**': []}
 html_title = ""
 html_js_files = ['hide-version.js']
